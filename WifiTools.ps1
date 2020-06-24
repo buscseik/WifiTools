0a9b8b864002599a27fdeb8416bdae4621249eab
buscseik <buscseik@gmail.com>
2020-06-24 13:19:30 +0100

    Adding minor fix and update wifi profile:

--- WifiTools.ps1
+++ WifiTools.ps1
@@ -1452,7 +1452,7 @@ function Create-W4AWifiProfile()
         return $RuntimeParameterDictionary
     }
     begin{
-        $PHYType = $PsBoundParameters[$ParameterName]
+        
         $SelectedInterface=$PsBoundParameters[$ParameterNameInterface]
     }
     process{
@@ -1464,38 +1464,74 @@ function Create-W4AWifiProfile()
 $XMLProfile= @"
 <?xml version="1.0"?>
 <WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1">
-	<name>$NetworkName</name>
+	<name>Horizon Wi-Free</name>
 	<SSIDConfig>
 		<SSID>
-
-			<name>$NetworkName</name>
+			<hex>486F72697A6F6E2057692D46726565</hex>
+			<name>Horizon Wi-Free</name>
 		</SSID>
-		<nonBroadcast>false</nonBroadcast>
 	</SSIDConfig>
 	<connectionType>ESS</connectionType>
 	<connectionMode>manual</connectionMode>
-	<autoSwitch>false</autoSwitch>
 	<MSM>
 		<security>
 			<authEncryption>
 				<authentication>WPA2</authentication>
 				<encryption>AES</encryption>
 				<useOneX>true</useOneX>
-				<FIPSMode xmlns="http://www.microsoft.com/networking/WLAN/profile/v2">false</FIPSMode>
 			</authEncryption>
 			<PMKCacheMode>enabled</PMKCacheMode>
 			<PMKCacheTTL>720</PMKCacheTTL>
 			<PMKCacheSize>128</PMKCacheSize>
 			<preAuthMode>disabled</preAuthMode>
 			<OneX xmlns="http://www.microsoft.com/networking/OneX/v1">
-				<cacheUserData>true</cacheUserData>
 				<authMode>user</authMode>
-				<EAPConfig><EapHostConfig xmlns="http://www.microsoft.com/provisioning/EapHostConfig"><EapMethod><Type xmlns="http://www.microsoft.com/provisioning/EapCommon">25</Type><VendorId xmlns="http://www.microsoft.com/provisioning/EapCommon">0</VendorId><VendorType xmlns="http://www.microsoft.com/provisioning/EapCommon">0</VendorType><AuthorId xmlns="http://www.microsoft.com/provisioning/EapCommon">0</AuthorId></EapMethod><Config xmlns="http://www.microsoft.com/provisioning/EapHostConfig"><Eap xmlns="http://www.microsoft.com/provisioning/BaseEapConnectionPropertiesV1"><Type>25</Type><EapType xmlns="http://www.microsoft.com/provisioning/MsPeapConnectionPropertiesV1"><ServerValidation><DisableUserPromptForServerValidation>false</DisableUserPromptForServerValidation><ServerNames></ServerNames></ServerValidation><FastReconnect>true</FastReconnect><InnerEapOptional>false</InnerEapOptional><Eap xmlns="http://www.microsoft.com/provisioning/BaseEapConnectionPropertiesV1"><Type>26</Type><EapType xmlns="http://www.microsoft.com/provisioning/MsChapV2ConnectionPropertiesV1"><UseWinLogonCredentials>false</UseWinLogonCredentials></EapType></Eap><EnableQuarantineChecks>false</EnableQuarantineChecks><RequireCryptoBinding>false</RequireCryptoBinding><PeapExtensions><PerformServerValidation xmlns="http://www.microsoft.com/provisioning/MsPeapConnectionPropertiesV2">false</PerformServerValidation><AcceptServerName xmlns="http://www.microsoft.com/provisioning/MsPeapConnectionPropertiesV2">false</AcceptServerName><PeapExtensionsV2 xmlns="http://www.microsoft.com/provisioning/MsPeapConnectionPropertiesV2"><AllowPromptingWhenServerCANotFound xmlns="http://www.microsoft.com/provisioning/MsPeapConnectionPropertiesV3">true</AllowPromptingWhenServerCANotFound></PeapExtensionsV2></PeapExtensions></EapType></Eap></Config></EapHostConfig></EAPConfig>
+				<EAPConfig>
+					<EapHostConfig xmlns="http://www.microsoft.com/provisioning/EapHostConfig">
+						<EapMethod>
+							<Type xmlns="http://www.microsoft.com/provisioning/EapCommon">25</Type>
+							<VendorId xmlns="http://www.microsoft.com/provisioning/EapCommon">0</VendorId>
+							<VendorType xmlns="http://www.microsoft.com/provisioning/EapCommon">0</VendorType>
+							<AuthorId xmlns="http://www.microsoft.com/provisioning/EapCommon">0</AuthorId>
+						</EapMethod>
+						<Config xmlns="http://www.microsoft.com/provisioning/EapHostConfig">
+							<Eap xmlns="http://www.microsoft.com/provisioning/BaseEapConnectionPropertiesV1">
+								<Type>25</Type>
+								<EapType xmlns="http://www.microsoft.com/provisioning/MsPeapConnectionPropertiesV1">
+									<ServerValidation>
+										<DisableUserPromptForServerValidation>false</DisableUserPromptForServerValidation>
+										<ServerNames></ServerNames>
+										<TrustedRootCA>16 2e f1 3e 10 08 15 e6 2d 69 6d db 63 ee d8 38 90 a3 68 0f </TrustedRootCA>
+									</ServerValidation>
+									<FastReconnect>true</FastReconnect>
+									<InnerEapOptional>false</InnerEapOptional>
+									<Eap xmlns="http://www.microsoft.com/provisioning/BaseEapConnectionPropertiesV1">
+										<Type>26</Type>
+										<EapType xmlns="http://www.microsoft.com/provisioning/MsChapV2ConnectionPropertiesV1">
+											<UseWinLogonCredentials>false</UseWinLogonCredentials>
+										</EapType>
+									</Eap>
+									<EnableQuarantineChecks>false</EnableQuarantineChecks>
+									<RequireCryptoBinding>false</RequireCryptoBinding>
+									<PeapExtensions>
+										<PerformServerValidation xmlns="http://www.microsoft.com/provisioning/MsPeapConnectionPropertiesV2">true</PerformServerValidation>
+										<AcceptServerName xmlns="http://www.microsoft.com/provisioning/MsPeapConnectionPropertiesV2">true</AcceptServerName>
+										<PeapExtensionsV2 xmlns="http://www.microsoft.com/provisioning/MsPeapConnectionPropertiesV2">
+											<AllowPromptingWhenServerCANotFound xmlns="http://www.microsoft.com/provisioning/MsPeapConnectionPropertiesV3">true</AllowPromptingWhenServerCANotFound>
+										</PeapExtensionsV2>
+									</PeapExtensions>
+								</EapType>
+							</Eap>
+						</Config>
+					</EapHostConfig>
+				</EAPConfig>
 			</OneX>
 		</security>
 	</MSM>
+	<MacRandomization xmlns="http://www.microsoft.com/networking/WLAN/profile/v3">
+		<enableRandomization>false</enableRandomization>
+	</MacRandomization>
 </WLANProfile>
-
 "@
 
         $currentlocation=Get-Location
@@ -2096,13 +2132,23 @@ Function Monitor-WifiState()
 
             if($SelectedInterface -eq $null)
             {
-                $CurrentState=Get-WifiState
-                DateEcho $CurrentState.LogLine
+                try{
+                    $CurrentState=Get-WifiState
+                    DateEcho $CurrentState.LogLine
+                }
+                Catch{
+                    DateEcho "No connection"
+                }
             }
             else
             {
-                $CurrentState=Get-WifiState -InterfaceName $SelectedInterface
-                DateEcho $CurrentState.LogLine
+                try{
+                    $CurrentState=Get-WifiState -InterfaceName $SelectedInterface
+                    DateEcho $CurrentState.LogLine
+                }
+                Catch{
+                    DateEcho "No connection"
+                }
             }
 
             Sleep $CheckTime
